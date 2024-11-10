3069bb8c803179e848103139651db54cd2eb8cb5
VidushiRaghuvanshi <vidushiraghuvanshi062@gmail.com>
2024-11-10 16:47:14 +0530

    sets in python

---
+++ sets.py
@@ -0,0 +1,8 @@
+set={1,2,3,4,5,1,2,3,4,5,1,2,3,4,5}
+print(set)
+set1=set
+set2={6,7,8,9}
+print("set1:",set1)
+print("set2:",set2)
+print(set1.symmetric_difference(set2))
+print(set1.difference(set2))
